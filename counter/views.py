c6c1b855c5abb39087007ec73ae255593b8be74c
Fang1331 <aryan.rai1331@gmail.com>
2025-03-12 00:30:37 +0530

    Update views.py

--- counter/views.py
+++ counter/views.py
@@ -9,7 +9,7 @@ def home(request):
     if request.method=='POST':
         query=request.POST['query']
         api_url='https://api.api-ninjas.com/v1/nutrition?query='
-        api_request = requests.get(api_url + query,headers={'X-Api-Key': 'Wn0jsnoMcqB/YDTD1KcbNg==rP0A5RMNFd9SJjh8'})
+        api_request = requests.get(api_url + query,headers={'X-Api-Key': 'API KEY'}) #Write your API key in place of API key
 
         try:
             api=json.loads(api_request.content)
