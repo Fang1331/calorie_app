7a7f70656424ed8188863a026ac116e1da9b3f71
Fang1331 <aryan.rai1331@gmail.com>
2025-03-11 20:15:31 +0530

    Initial commit

---
+++ counter/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+#Wn0jsnoMcqB/YDTD1KcbNg==rP0A5RMNFd9SJjh8
+def home(request):
+    import requests
+    import json
+
+    if request.method=='POST':
+        query=request.POST['query']
+        api_url='https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(api_url + query,headers={'X-Api-Key': 'Wn0jsnoMcqB/YDTD1KcbNg==rP0A5RMNFd9SJjh8'})
+
+        try:
+            api=json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api="Error found"
+            print(e)
+        return render(request,'home.html',{'api':api})
+    else:
+        return render(request,'home.html',{'query':'Enter a valid query'})
+        
